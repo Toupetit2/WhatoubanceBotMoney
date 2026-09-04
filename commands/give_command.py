07ee5a6aca8f776d52b8a9eb9516b7a175eea489
Toupetit2 <theo.doublet@yahoo.com>
2026-09-04 21:47:52 +0200

    Update give_command.py

--- commands/give_command.py
+++ commands/give_command.py
@@ -1,6 +1,9 @@
 import discord
 from discord import app_commands
 import give
+import os
+
+GUILD_ID = os.environ.get("GUILD_ID")
 
 def setup(bot):
     @app_commands.guild_only()
@@ -13,3 +16,16 @@ def setup(bot):
             await interaction.response.send_message(f"✅{member.display_name} a reçu {amount} coins, il en a maintenant {new_balance}", ephemeral=True)
         else:
             await interaction.response.send_message(f"✅{member.display_name} a perdu {-amount} coins, il en a maintenant {new_balance}", ephemeral=True)
+
+    @app_commands.guild_only()
+    @app_commands.default_permissions(administrator=True)
+    @bot.tree.command(name="give_coins_everyone", description="Donne X coins à tous les membres du serveur")
+    async def give_coins_everyone_command(interaction: discord.Interaction, member: discord.Member, amount: int):
+        await interaction.response.defer(ephemeral=True)
+        guild = bot.get_guild(int(GUILD_ID))
+        for member in guild.members:
+            give.give_coins(amount, member)
+            if amount > 0:
+                await interaction.followup.send(f"✅ Tous les membres ont reçu {amount} coins", ephemeral=True)
+            else:
+                await interaction.followup.send(f"✅ Tous les membres ont perdu {-amount} coins", ephemeral=True)
