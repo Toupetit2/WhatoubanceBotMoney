35450265f0b82343b4c9084fa864a37ae31604cc
Toupetit2 <113516234+Toupetit2@users.noreply.github.com>
2026-09-17 12:59:45 +0200

    Update minigame_limits_command.py

--- minigame_limits_command.py
+++ minigame_limits_command.py
@@ -7,7 +7,7 @@ def setup(bot):
     @app_commands.guild_only()
     @app_commands.default_permissions(administrator=True)
     @bot.tree.command(name="minigame_limit_change_status", description="Change la limite pour les minijeu : illimité/1 par jour")
-    async def status_update_command(ctx, member: discord.Member):
+    async def status_update_command(interaction: discord.Interaction, member: discord.Member):
         nouveau = STATUS_DEFAULT if has_unlimited(member.id) else STATUS_UNLIMITED
         set_status(member.id, nouveau)
 
